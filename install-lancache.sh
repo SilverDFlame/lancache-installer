f4b44ac693b1b0265bbe67f2fc46f8ebff2216d7
Andrew Allen <darkvivix@gmail.com>
2020-03-25 13:40:32 -0700

    updated nginx package source to debian from ubuntu

--- install-lancache.sh
+++ install-lancache.sh
@@ -46,8 +46,8 @@ if [[ -d /etc/apt/sources.list.d/nginx.list ]]; then
 else
  	echo "Adding NGINX repository to Ubuntu repository"
  	curl -s https://nginx.org/keys/nginx_signing.key | apt-key add -
- 	echo "deb [arch=amd64] https://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx" > /etc/apt/sources.list.d/nginx.list
- 	echo "deb-src https://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx" >> /etc/apt/sources.list.d/nginx.list
+ 	echo "deb [arch=amd64] https://nginx.org/packages/mainline/debian/ `lsb_release -cs` nginx" > /etc/apt/sources.list.d/nginx.list
+ 	echo "deb-src https://nginx.org/packages/mainline/debian/ `lsb_release -cs` nginx" >> /etc/apt/sources.list.d/nginx.list
 
 fi
 
